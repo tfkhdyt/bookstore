313d04d68f7b6674e2744f9e8e414c582c5200de
tfkhdyt <tfkhdyt@pm.me>
2022-06-13 06:05:30 +0700

    Add: FindBook, UpdateBook, and DeleteBook

--- api/controllers/books.go
+++ api/controllers/books.go
@@ -1,18 +1,25 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/tfkhdyt/bookstore/api/models"
 )
 
+// structs
 type CreateBookInput struct {
 	Title  string `json:"title" binding:"required"`
 	Author string `json:"author" binding:"required"`
 }
 
-// /GET books
+type UpdateBookInput struct {
+	Title  string `json:"title"`
+	Author string `json:"author"`
+}
+
+// GET /books
 // Get all books
 func FindBooks(ctx *gin.Context) {
 	var books []models.Book
@@ -23,7 +30,7 @@ func FindBooks(ctx *gin.Context) {
 	})
 }
 
-// /POST books
+// POST /books
 // Create a new book
 func CreateBook(ctx *gin.Context) {
 	// validate input
@@ -44,5 +51,66 @@ func CreateBook(ctx *gin.Context) {
 
 	ctx.JSON(http.StatusCreated, gin.H{
 		"data": book,
+	})
+}
+
+// GET /books/:id
+// Find a book
+func FindBook(ctx *gin.Context) {
+	var book models.Book
+
+	if err := models.DB.Where("id = ?", ctx.Param("id")).First(&book).Error; err != nil {
+		ctx.JSON(http.StatusNotFound, gin.H{
+			"error": "Book not found",
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"data": book,
+	})
+}
+
+// PATCH /books/:id
+// Update book
+func UpdateBook(ctx *gin.Context) {
+	// get model if exist
+	var book models.Book
+	if err := models.DB.Where("id = ?", ctx.Param("id")).First(&book).Error; err != nil {
+		ctx.JSON(http.StatusNotFound, gin.H{
+			"error": "Book not found",
+		})
+		return
+	}
+
+	// validate input
+	var input UpdateBookInput
+	if err := ctx.ShouldBindJSON(&input); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	models.DB.Model(&book).Updates(input)
+	ctx.JSON(http.StatusOK, gin.H{
+		"data": book,
+	})
+}
+
+// DELETE /books/:id
+func DeleteBook(c *gin.Context) {
+	var book models.Book
+
+	if err := models.DB.Where("id = ?", c.Param("id")).First(&book).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	models.DB.Delete(&book)
+	c.JSON(http.StatusOK, gin.H{
+		"message": fmt.Sprintf("Book with id %v deleted successfully", c.Param("id")),
 	})
 }
--- api/main.go
+++ api/main.go
@@ -20,6 +20,10 @@ func main() {
 	})
 
 	r.GET("/books", controllers.FindBooks)
+	r.GET("/books/:id", controllers.FindBook)
+	r.POST("/books", controllers.CreateBook)
+	r.PATCH("/books/:id", controllers.UpdateBook)
+	r.DELETE("/books/:id", controllers.DeleteBook)
 
 	r.Run()
 }
