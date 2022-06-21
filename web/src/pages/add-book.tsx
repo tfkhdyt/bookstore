5d38b2c324344090ada8999072da635304b24d36
tfkhdyt <tfkhdyt@pm.me>
2022-06-21 21:34:41 +0700

    fix: fix height of publisher and description field in add-book

--- web/src/pages/add-book.tsx
+++ web/src/pages/add-book.tsx
@@ -130,7 +130,7 @@ const AddBook = () => {
                 id='publisher'
                 type='text'
                 {...formik.getFieldProps('publisher')}
-                className='mt-2 block h-32 w-full rounded-md border border-gray-200 bg-white px-4 py-2 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-300 dark:focus:border-blue-300'
+                className='mt-2 block w-full rounded-md border border-gray-200 bg-white px-4 py-2 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-300 dark:focus:border-blue-300'
               />
             </div>
             <div>
@@ -168,7 +168,7 @@ const AddBook = () => {
               <textarea
                 id='description'
                 {...formik.getFieldProps('description')}
-                className='mt-2 block w-full rounded-md border border-gray-200 bg-white px-4 py-2 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-300 dark:focus:border-blue-300'
+                className='mt-2 block h-32 w-full rounded-md border border-gray-200 bg-white px-4 py-2 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-300 dark:focus:border-blue-300'
               />
             </div>
             <div>
