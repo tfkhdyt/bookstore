be72d4921e25c8fc7d86aba152fd654da06df5ef
tfkhdyt <tfkhdyt@pm.me>
2022-06-25 17:18:16 +0700

    fix(fetcher): change fetcher return data

--- web/src/lib/fetcher.ts
+++ web/src/lib/fetcher.ts
@@ -4,5 +4,5 @@ export const fetcher = async (url: string) => {
   const { data } = await axiosInstance.get(url).catch((err) => {
     return err.message;
   });
-  return data.data;
+  return data;
 };
