59256596abdd4ce7a4462a3d7455314e7ee09b7d
tfkhdyt <tfkhdyt@pm.me>
2022-06-25 14:59:00 +0700

    feat(api): add pagination

--- api/controllers/books.go
+++ api/controllers/books.go
@@ -2,9 +2,9 @@ package controllers
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"strconv"
-	"strings"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -37,6 +37,35 @@ type UpdateBookInput struct {
 func FindBooks(c *gin.Context) {
 	var books []models.Book
 
+	page := c.Query("page")
+	limit := c.Query("limit")
+
+	if page != "" && limit != "" {
+		page, err := strconv.Atoi(page)
+		if err != nil {
+			log.Panicln(err.Error())
+		}
+
+		limit, err := strconv.Atoi(limit)
+		if err != nil {
+			log.Panicln(err.Error())
+		}
+
+		if err := models.DB.Limit(limit).Offset(limit * (page - 1)).Find(&books).Error; err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		fmt.Println("Paginated books printed!")
+		c.JSON(http.StatusOK, gin.H{
+			"data":   books,
+			"length": len(books),
+		})
+		return
+	}
+
 	if err := models.DB.Find(&books).Error; err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
 			"error": err.Error(),
@@ -45,57 +74,57 @@ func FindBooks(c *gin.Context) {
 	}
 
 	// query checking
-	var filteredBooks []models.Book
-	fmt.Println(c.Params)
-	if c.Query("title") != "" {
-		for _, book := range books {
-			if strings.Contains(strings.ToLower(book.Title), strings.ToLower(c.Query("title"))) {
-				filteredBooks = append(filteredBooks, book)
-			}
-		}
-	}
-	if c.Query("author") != "" {
-		for _, book := range books {
-			if strings.Contains(strings.ToLower(book.Author), strings.ToLower(c.Query("author"))) {
-				filteredBooks = append(filteredBooks, book)
-			}
-		}
-	}
-	if c.Query("isbn") != "" {
-		for _, book := range books {
-			if strings.Contains(strings.ToLower(book.ISBN), strings.ToLower(c.Query("isbn"))) {
-				filteredBooks = append(filteredBooks, book)
-			}
-		}
-	}
-	if c.Query("publisher") != "" {
-		for _, book := range books {
-			if strings.Contains(strings.ToLower(book.Publisher), strings.ToLower(c.Query("publisher"))) {
-				filteredBooks = append(filteredBooks, book)
-			}
-		}
-	}
-	if c.Query("numberOfPages") != "" {
-		numberOfPages, err := strconv.ParseInt(c.Query("numberOfPages"), 10, 64)
-		if err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{
-				"error": err.Error(),
-			})
-		}
-		for _, book := range books {
-			if book.NumberOfPages == uint(numberOfPages) {
-				filteredBooks = append(filteredBooks, book)
-			}
-		}
-	}
+	// var filteredBooks []models.Book
+	// fmt.Println(c.Queries)
+	// if c.Query("title") != "" {
+	// 	for _, book := range books {
+	// 		if strings.Contains(strings.ToLower(book.Title), strings.ToLower(c.Query("title"))) {
+	// 			filteredBooks = append(filteredBooks, book)
+	// 		}
+	// 	}
+	// }
+	// if c.Query("author") != "" {
+	// 	for _, book := range books {
+	// 		if strings.Contains(strings.ToLower(book.Author), strings.ToLower(c.Query("author"))) {
+	// 			filteredBooks = append(filteredBooks, book)
+	// 		}
+	// 	}
+	// }
+	// if c.Query("isbn") != "" {
+	// 	for _, book := range books {
+	// 		if strings.Contains(strings.ToLower(book.ISBN), strings.ToLower(c.Query("isbn"))) {
+	// 			filteredBooks = append(filteredBooks, book)
+	// 		}
+	// 	}
+	// }
+	// if c.Query("publisher") != "" {
+	// 	for _, book := range books {
+	// 		if strings.Contains(strings.ToLower(book.Publisher), strings.ToLower(c.Query("publisher"))) {
+	// 			filteredBooks = append(filteredBooks, book)
+	// 		}
+	// 	}
+	// }
+	// if c.Query("numberOfPages") != "" {
+	// 	numberOfPages, err := strconv.ParseInt(c.Query("numberOfPages"), 10, 64)
+	// 	if err != nil {
+	// 		c.JSON(http.StatusBadRequest, gin.H{
+	// 			"error": err.Error(),
+	// 		})
+	// 	}
+	// 	for _, book := range books {
+	// 		if book.NumberOfPages == uint(numberOfPages) {
+	// 			filteredBooks = append(filteredBooks, book)
+	// 		}
+	// 	}
+	// }
 
-	if len(filteredBooks) > 0 {
-		fmt.Println("Filtered books printed!")
-		c.JSON(http.StatusOK, gin.H{
-			"data": filteredBooks,
-		})
-		return
-	}
+	// if len(filteredBooks) > 0 {
+	// 	fmt.Println("Filtered books printed!")
+	// 	c.JSON(http.StatusOK, gin.H{
+	// 		"data": filteredBooks,
+	// 	})
+	// 	return
+	// }
 
 	fmt.Println("All books printed!")
 	c.JSON(http.StatusOK, gin.H{
