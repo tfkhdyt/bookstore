33a699d9e942b7221b8328085fdf362b090ade84
tfkhdyt <tfkhdyt@pm.me>
2022-06-30 17:22:06 +0700

    fix(uploadImage): hide cloudinary upload preset and cloud name to env

--- web/src/lib/uploadImage.ts
+++ web/src/lib/uploadImage.ts
@@ -1,17 +1,23 @@
 import axios from 'axios';
 
 export const uploadImage = async (image: File) => {
+  const UPLOAD_PRESET = process.env
+    .NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET as string;
+  const CLOUD_NAME = process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME as string;
+
+  // console.log(UPLOAD_PRESET, CLOUD_NAME);
+
   const data = new FormData();
   data.append('file', image);
-  data.append('upload_preset', 'tfkhdyt');
-  data.append('cloud_name', 'tfkhdyt');
+  data.append('upload_preset', UPLOAD_PRESET);
+  data.append('cloud_name', CLOUD_NAME);
   // fetch('https://api.cloudinary.com/v1_1/tfkhdyt/image/upload', {
   //   method: 'post',
   //   body: data,
   // });
 
   const result = await axios
-    .post('https://api.cloudinary.com/v1_1/tfkhdyt/image/upload', data)
+    .post(`https://api.cloudinary.com/v1_1/${CLOUD_NAME}/image/upload`, data)
     .catch((err) => {
       console.error(err);
     });
