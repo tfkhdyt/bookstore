91194c28cd30db5e7106093851e1a9e312d05d16
tfkhdyt <tfkhdyt@pm.me>
2022-07-06 09:09:10 +0700

    feat: add predefined return var

--- api/config/config.go
+++ api/config/config.go
@@ -6,13 +6,13 @@ import (
 	"github.com/joho/godotenv"
 )
 
-func GetDBInfo() (string, string, string, string) {
+func GetDBInfo() (dbHost string, dbUser string, dbPwd string, dbName string) {
 	godotenv.Load()
 
-	dbHost := os.Getenv("DB_HOST")
-	dbUser := os.Getenv("DB_USER")
-	dbPwd := os.Getenv("DB_PWD")
-	dbName := os.Getenv("DB_NAME")
+	dbHost = os.Getenv("DB_HOST")
+	dbUser = os.Getenv("DB_USER")
+	dbPwd = os.Getenv("DB_PWD")
+	dbName = os.Getenv("DB_NAME")
 
-	return dbHost, dbUser, dbPwd, dbName
+	return
 }
--- api/db/setup.go
+++ api/db/setup.go
@@ -10,11 +10,11 @@ import (
 	// _ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
-func ConnectDatabase() (*gorm.DB, error) {
+func ConnectDatabase() (db *gorm.DB, err error) {
 	dbHost, dbUser, dbPwd, dbName := config.GetDBInfo()
 
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=5432 sslmode=disable TimeZone=Asia/Jakarta", dbHost, dbUser, dbPwd, dbName)
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	// database, err := gorm.Open("sqlite3", "test.db")
 
 	// if err != nil {
