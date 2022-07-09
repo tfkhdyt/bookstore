0318dd1614eb3f8868a93c076f55dedcfa3e35bf
tfkhdyt <tfkhdyt@pm.me>
2022-07-09 08:21:36 +0700

    refactor(delete controller): implement service

--- api/repositories/books/delete.controller.go
+++ api/repositories/books/delete.controller.go
@@ -2,24 +2,32 @@ package books
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"github.com/tfkhdyt/bookstore/api/models"
+	booksServices "github.com/tfkhdyt/bookstore/api/services/books"
 )
 
 // DELETE /books/:id
 func (repo BooksRepository) DeleteBook(c *gin.Context) {
 	var book models.Book
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		log.Fatalln(err.Error())
+		return
+	}
 
-	if err := repo.DB.First(&book, c.Param("id")).Error; err != nil {
+	if err := booksServices.FindBook(repo.DB, &book, id); err != nil {
 		c.JSON(http.StatusNotFound, gin.H{
 			"error": err.Error(),
 		})
 		return
 	}
 
-	if err := repo.DB.Delete(&book).Error; err != nil {
+	if err := booksServices.DeleteBook(repo.DB, &book); err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
 			"error": err.Error(),
 		})
