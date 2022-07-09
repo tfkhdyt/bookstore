67cb36ed63f1b1f22431056ae1eaa328e053abde
tfkhdyt <tfkhdyt@pm.me>
2022-07-09 09:34:40 +0700

    refactor(detail); migrate to mantine

--- web/src/pages/books/[id]/index.tsx
+++ web/src/pages/books/[id]/index.tsx
@@ -1,4 +1,14 @@
-import { Anchor, Breadcrumbs } from '@mantine/core';
+import {
+  Anchor,
+  Box,
+  Breadcrumbs,
+  Grid,
+  List,
+  Space,
+  Text,
+  Title,
+} from '@mantine/core';
+import { useMediaQuery } from '@mantine/hooks';
 import { format } from 'date-fns';
 import Head from 'next/head';
 import Image from 'next/image';
@@ -22,6 +32,7 @@ const Detail = () => {
   const router = useRouter();
   const { id } = router.query;
   const [bookID] = useState(id);
+  const isBreakpointSm = useMediaQuery('(max-width: 768px)');
 
   const { data, error, mutate } = useSWR<IFetcher>(
     bookID ? `/books/${bookID}` : null,
@@ -82,10 +93,97 @@ const Detail = () => {
             },
           ]}
         /> */}
-        <h1 className='mb-3 text-2xl font-semibold leading-tight'>
-          Book Detail: {book.title}
-        </h1>
-        <div className='grid grid-cols-2 gap-8'>
+        <Title order={2}>Book Detail: {book.title}</Title>
+        <Space h='md' />
+        <Grid grow align='flex-start' gutter={60}>
+          {/* Book Detail */}
+          <Grid.Col xs={12} lg={6}>
+            <List spacing='sm'>
+              <List.Item>
+                <Text size='lg' weight={600}>
+                  Title:
+                </Text>
+                <Text size='md'>{book.title}</Text>
+              </List.Item>
+              <List.Item>
+                <Text size='lg' weight={600}>
+                  Author:
+                </Text>
+                <Text size='md'>{book.author}</Text>
+              </List.Item>
+              <List.Item>
+                <Text size='lg' weight={600}>
+                  ISBN:
+                </Text>
+                <Text size='md'>{book.isbn}</Text>
+              </List.Item>
+              <List.Item>
+                <Text size='lg' weight={600}>
+                  Description:
+                </Text>
+                <Text size='md' sx={{ fontStyle: 'italic' }} align='justify'>
+                  {book.description}
+                </Text>
+              </List.Item>
+              <List.Item>
+                <Text size='lg' weight={600}>
+                  Publisher:
+                </Text>
+                <Text size='md'>{book.publisher}</Text>
+              </List.Item>
+              <List.Item>
+                <Text size='lg' weight={600}>
+                  Number of pages:
+                </Text>
+                <Text size='md'>{book.numberOfPages}</Text>
+              </List.Item>
+              <List.Item>
+                <Text size='lg' weight={600}>
+                  Created at:
+                </Text>
+                <Text size='md'>
+                  {format(new Date(book.CreatedAt), 'iiii, d MMMM y H:mm:ss')}
+                </Text>
+              </List.Item>
+              <List.Item>
+                <Text size='lg' weight={600}>
+                  Updated at:
+                </Text>
+                <Text size='md'>
+                  {format(new Date(book.UpdatedAt), 'iiii, d MMMM y H:mm:ss')}
+                </Text>
+              </List.Item>
+            </List>
+          </Grid.Col>
+          {/* Book Cover */}
+          <Grid.Col
+            xs={12}
+            lg={6}
+            sx={{
+              justifySelf: 'flex-start',
+              position: 'relative',
+              height: isBreakpointSm ? 400 : 700,
+            }}
+          >
+            {book.coverImage && (
+              <Image
+                src={book.coverImage}
+                alt={`${book.title} cover image`}
+                layout='fill'
+                objectFit='contain'
+              />
+            )}
+          </Grid.Col>
+        </Grid>
+        <Box sx={{ marginTop: isBreakpointSm ? '3.5rem' : '1rem' }}>
+          <UpdateButton id={book.ID as number} />
+          <DeleteButton
+            id={book.ID as number}
+            title={book.title}
+            mutate={mutate}
+          />
+        </Box>
+        {/* <div className='grid grid-cols-2 gap-8'>
           <div className='text-lg'>
             <ul>
               <li>Title: </li>
@@ -124,15 +222,7 @@ const Detail = () => {
               )}
             </div>
           </div>
-        </div>
-        <div className='float-left mt-4 flex space-x-2'>
-          <UpdateButton id={book.ID as number} />
-          <DeleteButton
-            id={book.ID as number}
-            title={book.title}
-            mutate={mutate}
-          />
-        </div>
+        </div> */}
       </main>
     </>
   );
