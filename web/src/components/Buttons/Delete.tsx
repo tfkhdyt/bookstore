a8387335c25691a18416704a4bdde2a7202957b9
tfkhdyt <tfkhdyt@pm.me>
2022-07-10 10:55:49 +0700

    refactor(delete notif): make delete notif on separate file fix(delete button): add more error handling style(delete):  remove comments

--- web/src/components/Buttons/Delete.tsx
+++ web/src/components/Buttons/Delete.tsx
@@ -1,10 +1,14 @@
 /* eslint-disable react/no-unescaped-entities */
 import { Button, Text } from '@mantine/core';
 import { useModals } from '@mantine/modals';
-import { showNotification, updateNotification } from '@mantine/notifications';
+import axios from 'axios';
 import { useRouter } from 'next/router';
 
 import { axiosInstance } from '@/lib/axios';
+import {
+  showDeleteNotif,
+  updateDeleteNotif,
+} from '@/lib/notifications/delete.notification';
 
 import TrashCanIcon from '../Icons/TrashCan';
 
@@ -19,34 +23,50 @@ const DeleteButton = ({ id, title }: DeleteButtonProps) => {
   const router = useRouter();
 
   const handleOnConfirm = async () => {
-    showNotification({
-      id: 'delete-data',
-      loading: true,
-      title: 'Deleting',
-      message: 'Delete is in progress...',
-      autoClose: false,
-      disallowClose: true,
-    });
+    showDeleteNotif();
 
     try {
       await axiosInstance.delete(`/books/${id}`);
       // mutate();
-      updateNotification({
-        id: 'delete-data',
+      updateDeleteNotif({
         color: 'green',
-        title: `Delete book success!`,
+        title: 'Delete book success!',
         message: `${title} deleted successfully`,
-        autoClose: 2000,
       });
+
       router.push('/books');
     } catch (err) {
-      updateNotification({
-        id: 'delete-data',
-        color: 'red',
-        title: `Delete book failed!`,
-        message: `${title} failed to delete`,
-        autoClose: 2000,
-      });
+      if (axios.isAxiosError(err)) {
+        switch (err.response?.status) {
+          case 400:
+            updateDeleteNotif({
+              color: 'red',
+              title: 'Failed to delete book!',
+              message: 'Failed to convert id to int',
+            });
+            break;
+          case 404:
+            updateDeleteNotif({
+              color: 'red',
+              title: 'Failed to delete book!',
+              message: 'Book not found',
+            });
+            break;
+          case 500:
+            updateDeleteNotif({
+              color: 'red',
+              title: 'Failed to delete book!',
+              message: 'Failed to delete book',
+            });
+            break;
+        }
+      } else {
+        updateDeleteNotif({
+          color: 'red',
+          title: 'Failed to delete book!',
+          message: `${title} failed to delete`,
+        });
+      }
     }
   };
 
@@ -79,25 +99,6 @@ const DeleteButton = ({ id, title }: DeleteButtonProps) => {
     >
       Delete
     </Button>
-    // <button
-    //   type='button'
-    //   className='mx-auto flex w-full items-center justify-center space-x-1 rounded bg-red-400 px-3 py-2 text-center font-semibold text-white transition-colors duration-300 ease-in-out hover:bg-red-500 active:bg-red-600'
-    //   onClick={async () => {
-    //     await deleteBook(id, title, mutate);
-    //   }}
-    // >
-    //   <svg
-    //     width='24'
-    //     height='24'
-    //     xmlns='http://www.w3.org/2000/svg'
-    //     viewBox='0 0 24 24'
-    //     className='h-4 w-4'
-    //     fill='currentColor'
-    //   >
-    //     <path d='M19 24h-14c-1.104 0-2-.896-2-2v-16h18v16c0 1.104-.896 2-2 2m-9-14c0-.552-.448-1-1-1s-1 .448-1 1v9c0 .552.448 1 1 1s1-.448 1-1v-9zm6 0c0-.552-.448-1-1-1s-1 .448-1 1v9c0 .552.448 1 1 1s1-.448 1-1v-9zm6-5h-20v-2h6v-1.5c0-.827.673-1.5 1.5-1.5h5c.825 0 1.5.671 1.5 1.5v1.5h6v2zm-12-2h4v-1h-4v1z' />
-    //   </svg>
-    //   <span>Delete</span>
-    // </button>
   );
 };
 
---
+++ web/src/lib/notifications/delete.notification.ts
@@ -0,0 +1,32 @@
+import { showNotification, updateNotification } from '@mantine/notifications';
+
+export const showDeleteNotif = () => {
+  showNotification({
+    id: 'delete-data',
+    loading: true,
+    title: 'Deleting',
+    message: 'Delete is in progress...',
+    autoClose: false,
+    disallowClose: true,
+  });
+};
+
+interface UpdateDeleteNotifParams {
+  color: string;
+  title: string;
+  message: string;
+}
+
+export const updateDeleteNotif = ({
+  color,
+  title,
+  message,
+}: UpdateDeleteNotifParams) => {
+  updateNotification({
+    id: 'delete-data',
+    color,
+    title,
+    message,
+    autoClose: 2000,
+  });
+};
