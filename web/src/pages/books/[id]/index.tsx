4b67740cddd0ad41a2dd1ca6ec28455b0ebbe155
tfkhdyt <tfkhdyt@pm.me>
2022-07-10 10:58:52 +0700

    fix(detail): change error message to be more concise

--- web/src/pages/books/[id]/index.tsx
+++ web/src/pages/books/[id]/index.tsx
@@ -22,6 +22,7 @@ import useSWR from 'swr';
 import DeleteButton from '@/components/Buttons/Delete';
 import UpdateButton from '@/components/Buttons/Update';
 import Error from '@/components/Error';
+import { FetchError } from '@/lib/error/FetchError';
 import { fetcher } from '@/lib/fetcher';
 import { Book } from '@/types/Book';
 
@@ -40,20 +41,12 @@ const Detail = () => {
     bookID ? fetcher : null
   );
 
-  // if (!data) {
-  //   return (
-  //     <div className='grid min-h-full min-w-full place-items-center'>
-  //       <Loading />
-  //     </div>
-  //   );
-  // }
-
   if (error) {
-    return (
-      <div className='grid min-h-full min-w-full place-items-center'>
-        <Error />
-      </div>
-    );
+    if (error instanceof FetchError) {
+      return <Error message={error.message} status={error.status as number} />;
+    } else {
+      return <Error />;
+    }
   }
 
   if (!data)
@@ -87,18 +80,6 @@ const Detail = () => {
             </Link>
           ))}
         </Breadcrumbs>
-        {/* <Breadcrumb
-          content={[
-            {
-              link: '/books',
-              label: 'Manage Books',
-            },
-            {
-              link: `/books/${book.id}`,
-              label: book.title,
-            },
-          ]}
-        /> */}
         <Title order={2}>Book Detail: {book.title}</Title>
         <Space h='md' />
         <Grid grow align='flex-start' gutter={60}>
@@ -191,46 +172,6 @@ const Detail = () => {
             // mutate={mutate}
           />
         </Box>
-        {/* <div className='grid grid-cols-2 gap-8'>
-          <div className='text-lg'>
-            <ul>
-              <li>Title: </li>
-              <p>{book.title}</p>
-              <li>Author: </li>
-              <p>{book.author}</p>
-              <li>ISBN: </li>
-              <p>{book.isbn}</p>
-              <li>Description:</li>
-              <p className='text-justify text-base'>{book.description}</p>
-              <li>Publisher: </li>
-              <p>{book.publisher}</p>
-              <li>Number of pages: </li>
-              <p>{book.numberOfPages}</p>
-              <li>Created at: </li>
-              <p>
-                {' '}
-                {format(new Date(book.CreatedAt), 'iiii, d MMMM y H:mm:ss')}
-              </p>
-              <li>Updated at: </li>
-              <p>
-                {' '}
-                {format(new Date(book.UpdatedAt), 'iiii, d MMMM y H:mm:ss')}
-              </p>
-            </ul>
-          </div>
-          <div>
-            <div className='relative h-screen w-full'>
-              {book.coverImage && (
-                <Image
-                  src={book.coverImage}
-                  alt={`${book.title} cover image`}
-                  layout='fill'
-                  objectFit='contain'
-                />
-              )}
-            </div>
-          </div>
-        </div> */}
       </main>
     </>
   );
