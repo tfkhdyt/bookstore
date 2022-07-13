f1dfa01b1129f7bfb0d19aaf71188e180c6da5cd
tfkhdyt <tfkhdyt@pm.me>
2022-07-13 08:53:51 +0700

    fix(axios): add API_URL for production

--- web/src/lib/axios.ts
+++ web/src/lib/axios.ts
@@ -1,5 +1,8 @@
 import axios from 'axios';
 
+const ENV_MODE = process.env.NEXT_PUBLIC_ENV_MODE;
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
 export const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: ENV_MODE === 'development' ? 'http://localhost:8080' : API_URL,
 });
