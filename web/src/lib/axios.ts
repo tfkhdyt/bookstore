03f99c37134d42c52c88a7656d92dcadd96386d7
tfkhdyt <tfkhdyt@pm.me>
2022-07-15 18:23:28 +0700

    refactor(axios instance): remove envMode

--- web/src/lib/axios.ts
+++ web/src/lib/axios.ts
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
-const ENV_MODE = process.env.NEXT_PUBLIC_ENV_MODE;
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export const axiosInstance = axios.create({
-  baseURL: ENV_MODE === 'development' ? 'http://localhost:8080' : API_URL,
+  baseURL: API_URL,
 });
