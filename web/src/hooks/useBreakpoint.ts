d310a134248b79bfa5d5c148e4cd2d9e5a4d5608
tfkhdyt <tfkhdyt@pm.me>
2022-07-18 21:29:23 +0700

    ✨ feat(usebreakpoint query): add isSm

--- web/src/hooks/useBreakpoint.ts
+++ web/src/hooks/useBreakpoint.ts
@@ -2,7 +2,8 @@ import { useMediaQuery } from '@mantine/hooks';
 
 export default function useBreakpoint() {
   const isXs = useMediaQuery('(min-width: 576px)');
+  const isSm = useMediaQuery('(min-width: 768px)');
   const isMd = useMediaQuery('(min-width: 992px)');
 
-  return { isXs, isMd };
+  return { isXs, isSm, isMd };
 }
